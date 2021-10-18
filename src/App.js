23b4dd9208376e2bf49bf096275583aea52ca1e1
Rajib Barua <rajibbaruaexp@gmail.com>
2021-10-19 00:07:37 +0600

    Header part updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./components/shared/Header/Header";
+import About from "./components/pages/About/About";
+import ContactUs from "./components/pages/ContactUs/ContactUs";
+import Home from "./components/pages/Home/Home";
+import Services from "./components/pages/Services/Services";
+import NotFound from "./components/pages/NotFound/NotFound";
 
+import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/services">
+            <Services></Services>
+          </Route>
+          <Route exact path="/contactus">
+            <ContactUs></ContactUs>
+          </Route>
+
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
